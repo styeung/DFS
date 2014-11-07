4c783b64b6af23eb3fccaffbe8e55e9b211d73d8
Sai To Yeung <saitoyeung@gmail.com>
2014-11-07 17:38:07 -0500

    Removes first drop players table migration

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ group :development do
 end
 
 gem 'nokogiri'
+gem 'newrelic_rpm'
--- db/migrate/20141105173454_drop_players_table.rb
+++
@@ -1,5 +0,0 @@
-class DropPlayersTable < ActiveRecord::Migration
-  def change
-    drop_table :players
-  end
-end
