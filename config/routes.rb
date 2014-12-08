5d866b3c1cbf98086a9048613f4865ba31687678
Sai To Yeung <saitoyeung@gmail.com>
2014-12-07 22:27:21 -0500

    Adds todays odds page

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   get "daily_starting_lineups", to: "players#daily_starting_lineups", as: "daily_starting_lineups"
   
   get "entire_day_games/:date", to: "games#entire_day_games", as: "entire_day_games"
+  
+  get "todays_odds", to: "games#todays_odds", as: "todays_odds"
 end
